e3f5778b7761536db89089019538dcfbce23fe98
nzhou <nzhou@iastate.edu>
2018-04-11 14:46:28 -0500

    dm update

--- c/double_metropolis.c
+++ c/double_metropolis.c
@@ -12,11 +12,34 @@
 #define MATHLIB_STANDALONE
 #include <Rmath.h>
 
-/**
- * This is the Q function
- **/
 double negpotential(double* w, double alpha, double eta, double tau2,); 
-double Hij(int i, int j, double alpha, double eta, double tau2);
+double Hij(double y_i, double  ystar_i, double y_j, double ystar_j, 
+		int i, int j, double alpha, double eta, double tau2);
+
+double data_pdf();
+double mixing_pdf();
+double prior_pdf();
+
+double posterior_pdf_w();
+/**
+ * Each of the parameters alpha, eta and tau2
+ * has two types of posteriors:
+ * 	- The multivariate normal -1
+ * 	- The negpotential function -2
+ * For the negpotential function type posterior,
+ * there are two methods to compute the intractable constant
+ * 	- The double- metropolis algorithm
+ * 	- importance sampling -3
+ * Only case 2 will be in this script
+ **/
+double posterior_pdf_alpha();
+double posterior_pdf_eta();
+double posterior_pdf_tau2();
+
+void dm_step1();
+void dm_step2();
+void dm_step3();
+
 
 
 int main(void){
