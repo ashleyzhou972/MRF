d9e0f594a2aefb9ea87f52ee10b6a1e3afa0daf2
Matt Schramm <mschramm@iastate.edu>
2018-05-04 18:11:07 -0500

    Coding style changes

--- c/dm_call.c
+++ c/dm_call.c
@@ -13,9 +13,10 @@ void allocate_column(double *w, double **w_bycol, int N, int T);
 void initialize(double alpha0, double eta0, double tau20, double *w,
 		double *alpha, double *eta, double *tau2, int N, int T);
 
-SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEXP bounds_alpha, SEXP bounds_eta, SEXP bounds_tau2, SEXP initials){
+SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEXP bounds_alpha, SEXP bounds_eta, SEXP bounds_tau2, SEXP initials)
+{
 	//declaration of variables;
-	int N,T;
+	int N, T;
 	int *T_p;
 /*	int *y_int;*/
 	double *y;
@@ -29,7 +30,6 @@ SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEX
 	//Check incoming SEXP types and extract data
 	if (!isInteger(T_in))
 		error("[ERROR] First argument must be interger");
-	
 	else {
 		T_p = INTEGER(T_in);
 		T = *T_p;
@@ -40,7 +40,9 @@ SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEX
 		y = REAL(y_in);
 		N = length(y_in);
 	}
+
 	int **neighbor_2d;
+
 	neighbor_2d = malloc(N*sizeof(int *));
 	for (int i = 0; i < N; ++i)
 		neighbor_2d[i] = malloc(N*sizeof(int));
@@ -49,26 +51,26 @@ SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEX
 	else {
 		neighbor_1d = INTEGER(neighbor_in);
 		dim_neighbor = length(neighbor_in);
-		if (dim_neighbor!=N*N)
+		if (dim_neighbor != N*N)
 			error("[ERROR] Size of neighborhood matrix does not agree with size of data vector");
 		else {
-			for (int i = 0;i<N;i++) {
-				for (int j = 0;j<N;j++) 
+			for (int i = 0; i < N; ++i) {
+				for (int j = 0; j < N; ++j)
 					neighbor_2d[i][j] = neighbor_1d[j*N+i];
 			}
 		}
 	}
-	if (!isReal(vars_in) || !isVector(vars_in) || !length(vars_in)==4)
+	if (!isReal(vars_in) || !isVector(vars_in) || !length(vars_in) == 4)
 		error("[ERROR] Fourth argument must be real vector with 4 elements");
-	else 
+	else
 		vars = REAL(vars_in);
-	if (!isReal(bounds_alpha) || !isReal(bounds_eta) || !isReal(bounds_tau2) 
+	if (!isReal(bounds_alpha) || !isReal(bounds_eta) || !isReal(bounds_tau2)
 			|| !isVector(bounds_alpha) || !isVector(bounds_eta) ||
-			!isVector(bounds_tau2)) 
+			!isVector(bounds_tau2))
 		error("[ERROR] three bounds arguments must be real vectors\n");
 	else {
-		if (!length(bounds_alpha)==2 || !length(bounds_eta)==2 ||
-					!length(bounds_tau2)==2)
+		if (!length(bounds_alpha) == 2 || !length(bounds_eta) == 2 ||
+					!length(bounds_tau2) == 2)
 			error("[ERROR] three bounds arguments must each be of length 2\n");
 
 		else {
@@ -85,29 +87,17 @@ SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEX
 		eta0 = initials_temp[1];
 		tau20 = initials_temp[2];
 	}
-	Rprintf("size of data vector is %d\n" , N);
-	Rprintf("number of iterations is %d\n" , T);
-	/*
-	Rprintf("data vector is :\n");
-	for (int i =0;i<N;i++) {
-		Rprintf("%d " , y_int[i]);
-	}
-	Rprintf("\n");
-	Rprintf("first column of neighbor matrix is :\n");
-	for (int i=0;i<N;i++){
-		Rprintf("%d ",neighbor_2d[i][0]);
-	}
-	Rprintf("\n");
-	*/
-	
+	Rprintf("size of data vector is %d\n", N);
+	Rprintf("number of iterations is %d\n", T);
+
 	SEXP R_alpha, R_eta, R_tau2, R_w, R_Return_List;
+
 	PROTECT(R_w = allocMatrix(REALSXP, N, T+1));
 	PROTECT(R_alpha = allocVector(REALSXP, T+1));
 	PROTECT(R_eta = allocVector(REALSXP, T+1));
 	PROTECT(R_tau2 = allocVector(REALSXP, T+1));
 	PROTECT(R_Return_List = allocVector(VECSXP, 4));
 	num_protected += 5;
-	
 
 /*	allocate(&w, N, T+1);*/
 	initialize(alpha0, eta0, tau20, REAL(R_w), REAL(R_alpha), REAL(R_eta), REAL(R_tau2), N, T+1);
@@ -127,13 +117,14 @@ SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEX
 	double *tau2 = REAL(R_tau2);
 	double *w = REAL(R_w);
 	double **w_bycol = malloc(T*sizeof **w_bycol);
+
 	allocate_column(w, w_bycol, N, T+1);
 	for (t = 0; t < T; ++t) {
 		printf("MC Iteration %d\n", t+1);
 		//step1;
 		metropolis_for_w_univar(t, N, w_bycol, y, vars[0]);
 		//step2 (alpha);
-		new_alpha = dm_step1(alpha[t], prior_alpha, vars[1],b_alpha);
+		new_alpha = dm_step1(alpha[t], prior_alpha, vars[1], b_alpha);
 		alpha[t+1] = dm_step2_t_alpha(t, w_bycol, alpha, eta, tau2, N, T, new_alpha, neighbor_2d);
 		//step3 (eta);
 		new_eta = dm_step1(eta[t], prior_eta, vars[2], b_eta);
@@ -142,7 +133,7 @@ SEXP double_metropolis(SEXP T_in, SEXP y_in, SEXP neighbor_in, SEXP vars_in, SEX
 		new_tau2 = dm_step1(tau2[t], prior_tau2, vars[3], b_tau2);
 		tau2[t+1] = dm_step2_t_tau2(t, w_bycol, alpha, eta, tau2, N, T, new_tau2, neighbor_2d);
 	}
-		
+
 	SET_VECTOR_ELT(R_Return_List, 0, R_w);
 	SET_VECTOR_ELT(R_Return_List, 1, R_alpha);
 	SET_VECTOR_ELT(R_Return_List, 2, R_eta);
